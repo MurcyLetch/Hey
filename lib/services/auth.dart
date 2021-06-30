7fb48f02ac0932f2a719d619e661d9b6e3d0a8b5
MurcyLetch <arijitindu99@gmail.com>
2021-06-30 19:57:27 +0530

    ...

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -15,8 +15,34 @@ class Authmethods{
       FirebaseUser firebaseUser=result.user;
       return _userFromFirebaseUser(firebaseUser);
     }catch(e){
-      print(e);
+      print(e.toString());
     }
 
+  }
+  Future signUpWithEmailAndPassword(String email,String password) async {
+    try{
+      AuthResult result=await _auth.createUserWithEmailAndPassword
+      (email: email, password: password);
+      FirebaseUser firebaseUser=result.user;
+      return _userFromFirebaseUser(firebaseUser);
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
+  Future resetPass(String email) async {
+    try{
+      return await _auth.sendPasswordResetEmail(email: email);
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
+  Future signOut()async{
+    try{
+      return await _auth.signOut();
+    }catch(e){
+      print(e.toString());
+    }
   }
 }
--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hey/services/auth.dart';
 import 'package:hey/widgets/widget.dart';
 class Signup extends StatefulWidget {
 
@@ -8,15 +9,36 @@ class Signup extends StatefulWidget {
 
 class _SignupState extends State<Signup> {
 
+  bool isloading=false;
+
+  Authmethods authmethods=new Authmethods();
+
+  final formkey= GlobalKey<FormState>();
   TextEditingController usernamectrl=TextEditingController();
   TextEditingController emailctrl=TextEditingController();
   TextEditingController passwordctrl=TextEditingController();
+
+  signmeup(){
+    if(formkey.currentState.validate()){
+      setState(() {
+        isloading=true;        
+      });
+
+      authmethods.signUpWithEmailAndPassword
+      (emailctrl.text, passwordctrl.text).then((val){
+        print("$val");
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbarmain(context),
-      body: SingleChildScrollView(
+      body: isloading ? Container(
+        child: Center(child: CircularProgressIndicator()),
+      ): SingleChildScrollView(
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 24),
           child: Column(
@@ -33,21 +55,41 @@ class _SignupState extends State<Signup> {
                 ),
               ),
               //),
-              TextField(
-                controller: usernamectrl,
-                style: simpletextfieldstyle(),
-                decoration: textfieldinputdecoration("user name"),
-              ),
+              Form(
 
-              TextField(
-                controller: emailctrl,
-                style: simpletextfieldstyle(),
-                decoration: textfieldinputdecoration("email"),
-              ),
-              TextField(
-                controller: passwordctrl,
-                style: simpletextfieldstyle(),
-                decoration: textfieldinputdecoration("password"),
+                key: formkey,
+
+                child: Column(
+                  children: [
+                TextFormField(
+                  validator: (val){
+                    return val.isEmpty || val.length<4 ? "user name length should be atleast 4 letters":null; 
+                  },
+                  controller: usernamectrl,
+                  style: simpletextfieldstyle(),
+                  decoration: textfieldinputdecoration("user name"),
+                ),
+
+                TextFormField(
+                  validator: (val){
+                    return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ?
+                          null : "Enter correct email";
+                  },
+                  controller: emailctrl,
+                  style: simpletextfieldstyle(),
+                  decoration: textfieldinputdecoration("email"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  validator: (val){
+                      return val.length < 6 ? "Password should consists atleast 6 characters" : null;
+                  },
+                  controller: passwordctrl,
+                  style: simpletextfieldstyle(),
+                  decoration: textfieldinputdecoration("password"),
+                ),
+                  ],
+                ),
               ),
 
               SizedBox(height: 10),
@@ -65,19 +107,24 @@ class _SignupState extends State<Signup> {
 
               SizedBox(height: 8),
 
-              Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(vertical: 18),
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: [
-                      const Color(0xff007EF4),
-                      const Color(0xff2A75BC),
-                    ]),
-                    borderRadius: BorderRadius.circular(30)),
-                child: Text(
-                  "Sign Up",
-                  style: mediumtextstyle(),
+              GestureDetector(
+                onTap: (){
+                  signmeup();
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width,
+                  padding: EdgeInsets.symmetric(vertical: 18),
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        const Color(0xff007EF4),
+                        const Color(0xff2A75BC),
+                      ]),
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Text(
+                    "Sign Up",
+                    style: mediumtextstyle(),
+                  ),
                 ),
               ),
               SizedBox(
